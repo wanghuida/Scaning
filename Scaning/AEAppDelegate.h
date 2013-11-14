dc5cb39c13da4fc9da443add6720151b338162b4
william <wanghuida258@126.com>
2013-11-14 09:40:32 +0800

    Initial Commit

---
+++ Scaning/AEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AEAppDelegate.h
+//  Scaning
+//
+//  Created by william on 13-11-14.
+//  Copyright (c) 2013年 iphone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Scaning/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Scaning
+//
+//  Created by william on 13-11-14.
+//  Copyright (c) 2013年 iphone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AEAppDelegate class]));
+    }
+}
